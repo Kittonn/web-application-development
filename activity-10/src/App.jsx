58f4fa54c11bebea24dd95f0c5c23219471d31f5
Kittonn <kitton.dev@gmail.com>
2024-02-12 14:05:53 +0700

    feat: activity 10

---
+++ activity-10/src/App.jsx
@@ -0,0 +1,11 @@
+import MyButton from "./components/MyButton";
+
+function App() {
+  return (
+    <>
+      <MyButton />
+    </>
+  );
+}
+
+export default App;
---
+++ activity-10/src/components/MyButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MyButton extends React.Component {
+  render() {
+    return <button type="button">Click Me!</button>;
+  }
+}
+
+export default MyButton;
